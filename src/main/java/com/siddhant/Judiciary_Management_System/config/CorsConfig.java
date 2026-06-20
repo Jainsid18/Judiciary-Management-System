27f425176d998529cbf552b8268964d448fe089c
Siddhant Jain <siddhantjain1811@gmail.com>
2026-06-21 03:48:02 +0530

    fix: cors + production api url

--- src/main/java/com/siddhant/Judiciary_Management_System/config/CorsConfig.java
+++ src/main/java/com/siddhant/Judiciary_Management_System/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://judiciary-management-frontend.vercel.app")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
